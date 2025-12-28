17817fd028d48eea42bba1cdf5d65e473d5c60ae
Neboer <rubinposter@gmail.com>
2025-12-28 20:56:59 +0800

    让skopeo同步时更加努力的retry。
    Increased retry times and added retry delay for skopeo sync.

--- skopeo-sync.sh
+++ skopeo-sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/skopeo sync --src yaml --dest docker --dest-tls-verify=false --retry-times 10 --scoped src-images.yaml "$TARGET_REPO"
+/usr/bin/skopeo sync --src yaml --dest docker --dest-tls-verify=false --retry-times 1000 --retry-delay 10s --scoped src-images.yaml "$TARGET_REPO"
