594aa836bad2da628ace20b53860f1893dbee7fa
Neboer <rubinposter@gmail.com>
2025-12-29 07:36:09 +0800

    命令失败显式错误，增加更多命令细节输出，打印getpeers结果

--- install-ssh.sh
+++ install-ssh.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+mkdir -p ~/.ssh
 echo "$PUBKEY"|tee -a ~/.ssh/authorized_keys > /dev/null
 sudo apt install -y openssh-server
 sudo systemctl enable ssh
--- install-yggdrasil-configure-network.sh
+++ install-yggdrasil-configure-network.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+set -euo pipefail
+
 curl -OL https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v0.5.12/yggdrasil-0.5.12-amd64.deb
 sudo DEBIAN_FRONTEND=noninteractive  apt install -y ./yggdrasil-0.5.12-amd64.deb
 yggdrasil -genconf | sudo tee /etc/yggdrasil.conf > /dev/null
+echo "yggdrasil.conf generated"
 sudo systemctl start yggdrasil
+echo "yggdrasil started"
+sleep 5
 sudo yggdrasilctl addpeer "$YGG_ENDPOINT"
+echo "Added Yggdrasil peer"
 echo "$YGG_HOSTS"|sudo tee -a /etc/hosts > /dev/null
+sleep 10
+sudo yggdrasilctl getpeers
