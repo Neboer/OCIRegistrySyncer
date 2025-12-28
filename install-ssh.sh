0db8078c02abdc55b8bb1e961cc3fcc77fc4c3a2
Neboer <rubinposter@gmail.com>
2025-12-29 07:24:01 +0800

    启动ssh服务器允许调试。

--- install-ssh.sh
+++ install-ssh.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 echo "$PUBKEY"|tee -a ~/.ssh/authorized_keys > /dev/null
+sudo apt install -y openssh-server
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
 ip addr show
